5508181ad8210200459b4323057c0b38052f30e7
Aditya Mishra <im.aditya707@gmail.com>
2026-04-18 11:40:12 +0530

    New driver switch logic

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public final class Constants {
+    private Constants()
+    {
+        // No need to create objeect for this class
+    }
+    public static final String BROWSER_NAME ="browserValue";
+    public static final String BROWSER_HEADLESS_ARG ="--headless";
+    public static final String BROWSER_DIS_GPU_ARG ="--disable-gpu";
+    public static final String BROWSER_RESOLUTION_ARG ="--window-size=1920,1080";
+
+}
--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,5 +1,7 @@
 package baseTest;
 
+import constants.Constants;
+import enums.BrowserEnum;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,12 +10,10 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.*;
-import java.util.Properties;
 
 public class BaseTest {
     public WebDriver driver;
@@ -21,43 +21,70 @@ public class BaseTest {
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws IOException {
 
-
-        if (System.getProperty("browserValue").equalsIgnoreCase("chrome"))
+        driver=switch (BrowserEnum.valueOf(System.getProperty(Constants.BROWSER_NAME).toUpperCase()))
         {
-            System.out.println("Running chrome browser");
-            ChromeOptions chromeOptions=new ChromeOptions();
-            chromeOptions.addArguments("--headless");
-            chromeOptions.addArguments("--disable-gpu");
-            chromeOptions.addArguments("--window-size=1920,1080");
-            WebDriverManager.chromedriver().setup();
-            driver=new ChromeDriver(chromeOptions);
-        } else if (System.getProperty("browserValue").equalsIgnoreCase("Firefox")) {
-            System.out.println("Running firefox browser");
-            FirefoxOptions firefoxOptions=new FirefoxOptions();
-            firefoxOptions.addArguments("--headless");
-            firefoxOptions.addArguments("--disable-gpu");
-            firefoxOptions.addArguments("--window-size=1920,1080");
-            WebDriverManager.firefoxdriver().setup();
-            driver=new FirefoxDriver(firefoxOptions);
-        } else {
-            System.out.println("Running edge browser");
-            EdgeOptions edgeOptions=new EdgeOptions();
-            edgeOptions.addArguments("--headless");
-            edgeOptions.addArguments("--disable-gpu");
-            edgeOptions.addArguments("--window-size=1920,1080");
-            WebDriverManager.edgedriver().setup();
-            driver=new EdgeDriver(edgeOptions);
-        }
+            case CHROME -> {
+                System.out.println("Running chrome browser");
+                yield initializeChromeDriver();
+            }
+            case FIREFOX -> {
+                System.out.println("Running firefox browser");
+                yield initializeFirefoxDriver();
+            }
+            case EDGE -> {
+                System.out.println("Running edge browser");
+                yield initializeEdgeDriver();
+            }
+            default -> throw new RuntimeException("Invalid BrowserName");
+        };
+
+//        if (System.getProperty(Constants.BROWSER_NAME).equalsIgnoreCase(BrowserEnum.CHROME.getBrowserName()))
+//        {
+//            driver=initializeChromeDriver();
+//
+//        } else if (System.getProperty(Constants.BROWSER_NAME).equalsIgnoreCase(BrowserEnum.FIREFOX.getBrowserName())) {
+//            driver=initializeFirefoxDriver();
+//        } else {
+//            driver=initializeEdgeDriver();
+//        }
 
         driver.manage().window().maximize();
 
     }
+
+    public static WebDriver initializeChromeDriver()
+    {
+        ChromeOptions chromeOptions=new ChromeOptions();
+        chromeOptions.addArguments(Constants.BROWSER_HEADLESS_ARG,Constants.BROWSER_DIS_GPU_ARG,Constants.BROWSER_RESOLUTION_ARG);
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(chromeOptions);
+    }
+
+    public static WebDriver initializeFirefoxDriver()
+    {
+        FirefoxOptions firefoxOptions=new FirefoxOptions();
+        firefoxOptions.addArguments(Constants.BROWSER_HEADLESS_ARG,Constants.BROWSER_DIS_GPU_ARG,Constants.BROWSER_RESOLUTION_ARG);
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver(firefoxOptions);
+    }
+
+    public static WebDriver initializeEdgeDriver()
+    {
+        EdgeOptions edgeOptions=new EdgeOptions();
+        edgeOptions.addArguments(Constants.BROWSER_HEADLESS_ARG,Constants.BROWSER_DIS_GPU_ARG,Constants.BROWSER_RESOLUTION_ARG);
+        WebDriverManager.edgedriver().setup();
+        return new EdgeDriver(edgeOptions);
+    }
+
+
 
     @AfterMethod(alwaysRun = true)
     public void tearDown()
     {
         driver.quit();
     }
+
+
 
 
 
